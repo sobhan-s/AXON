f58255eb929005917f880a2fe03649cd37e1504a
sobhan <sobhans@mindfiresolutions.com>
2026-03-09 18:05:26 +0530

    feat(api): add helmet in every serices

--- apps/projectService/src/app.ts
+++ apps/projectService/src/app.ts
@@ -3,6 +3,7 @@ import cookieparser from 'cookie-parser';
 import cors from 'cors';
 import { errorMiddleware } from '@dam/middlewares';
 import swaggerui from 'swagger-ui-express';
+import helmet from 'helmet';
 // import swaggerfile from "./swagger_output.json" with { type: "json" }
 
 const app: Express = express();
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: '8mb' }));
 app.use(cookieparser());
 app.use(errorMiddleware);
 app.set('trust proxy', 1);
+app.use(helmet())
 
 app.get('/hlth', (req, res) => {
   res.status(200).json({

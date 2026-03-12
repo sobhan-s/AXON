bb2e97744e49f612cf7d2b8e2c840a24234b89ab
sobhan <sobhans@mindfiresolutions.com>
2026-03-12 17:07:28 +0530

    feat(api): add report generation mail template

--- packages/mail/src/index.ts
+++ packages/mail/src/index.ts
@@ -1,4 +1,5 @@
 import { sendPasswordResetEmail } from './templates/reset.template.js';
 import { sendVerificationEmail } from './templates/verification.template.js';
+import { sendReportEmail } from './templates/report.template.js';
 
-export { sendPasswordResetEmail, sendVerificationEmail };
+export { sendPasswordResetEmail, sendVerificationEmail ,sendReportEmail};

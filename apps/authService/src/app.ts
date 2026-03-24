da667c4d562192076f8773fada3fc1010f61aa5d
sobhan <sobhans@mindfiresolutions.com>
2026-03-24 12:29:19 +0530

    fix(api): decrease the url limit 8mb to 1mb

--- apps/authService/src/app.ts
+++ apps/authService/src/app.ts
@@ -18,7 +18,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '8mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(cookieparser());
 app.use(errorMiddleware);
 app.set('trust proxy', 1);

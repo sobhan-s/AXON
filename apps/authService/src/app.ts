9a61a5a1f7fd7ca528305b24184c983e77e7867c
sobhan <sobhans@mindfiresolutions.com>
2026-03-24 12:25:21 +0530

    feat(api): rmmove the * from the cors options for security purpose

--- apps/authService/src/app.ts
+++ apps/authService/src/app.ts
@@ -10,7 +10,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', '*', 'http://auth-api:8001'],
+    origin: ['http://localhost:5173', 'http://auth-api:8001'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
